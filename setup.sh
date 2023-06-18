be03f16c819c26056b7def05b063e135a328078b
Vad <vadm.gagarin@ugmail.com>
2023-06-18 16:20:16 -0400

    init

---
+++ setup.sh
@@ -0,0 +1,20 @@
+echo -e "\n\ninstalling to ~/.config"
+echo "=============================="
+if [ ! -d $HOME/.config ]; then
+    echo "Creating ~/.config"
+    mkdir -p $HOME/.config
+fi
+# configs=$( find -path "$DOTFILES/config.symlink" -maxdepth 1 )
+DOTFILES=$(pwd)
+echo "DOTFILES=$DOTFILES"
+
+for config in $DOTFILES/config/*; do
+    target=$HOME/.config/$( basename $config )
+    if [ -e $target ]; then
+        echo "~${target#$HOME} already exists... Skipping."
+    else
+        echo "Creating symlink for $config"
+        ln -s $config $target
+    fi
+done
+
