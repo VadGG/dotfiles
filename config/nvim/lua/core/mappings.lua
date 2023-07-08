e37c969954500e576bdef2c0024a0e26b005a2c7
Vad <vadm.gagarin@ugmail.com>
2023-07-07 20:04:22 -0400

    restored reset hunk function

--- config/nvim/lua/core/mappings.lua
+++ config/nvim/lua/core/mappings.lua
@@ -271,6 +271,7 @@ M.telescope = {
     -- git
     ["<leader>cm"] = { "<cmd> Telescope git_commits <CR>", "Git commits" },
     ["<leader>gt"] = { "<cmd> Telescope git_status <CR>", "Git status" },
+    ["<leader>gb"] = { "<cmd> Telescope git_branches <CR>", "Git branches" },
 
     -- pick a hidden term
     ["<leader>pt"] = { "<cmd> Telescope terms <CR>", "Pick hidden term" },
@@ -425,14 +426,13 @@ M.gitsigns = {
     },
 
     -- Actions
-    -- TODO: change
-    -- ["<leader>rh"] = {
-    --   function()
-    --     require("gitsigns").reset_hunk()
-    --   end,
-    --   "Reset hunk",
-    -- },
-    --
+    ["<leader>rh"] = {
+      function()
+        require("gitsigns").reset_hunk()
+      end,
+      "Reset hunk",
+    },
+
     ["<leader>ph"] = {
       function()
         require("gitsigns").preview_hunk()
