3c7fe2ba41e752ea3e27e48f4bbd73fc988b589a
Vadim G <vadm.gagarin@ugmail.com>
2023-07-14 17:15:40 -0400

    added gitconfig and updated setup

--- setup.sh
+++ setup.sh
@@ -5,16 +5,33 @@ if [ ! -d $HOME/.config ]; then
     mkdir -p $HOME/.config
 fi
 # configs=$( find -path "$DOTFILES/config.symlink" -maxdepth 1 )
+
+shopt -s dotglob
 DOTFILES=$(pwd)
 echo "DOTFILES=$DOTFILES"
 
 for config in $DOTFILES/config/*; do
+    echo "config $config"
     target=$HOME/.config/$( basename $config )
+    echo "target $target"
     if [ -e $target ]; then
         echo "~${target#$HOME} already exists... Skipping."
     else
         echo "Creating symlink for $config"
         ln -s $config $target
+    fi
+done
+
+cd "$DOTFILES/homeconfig/"
+for hconfig in *; do
+    echo "config $hconfig"
+    target=$HOME/$( basename $hconfig )
+    echo "target $target"
+    if [ -e $target ]; then
+        echo "~${target#$HOME} already exists... Skipping."
+    else
+        echo "Creating symlink for $hconfig"
+        ln $hconfig $target
     fi
 done
 
