fb3268d2e53d8066fa6ea06950966be18d34f399
Vadim G <vadm.gagarin@ugmail.com>
2024-02-07 08:45:04 -0500

    added brew install file

---
+++ brew.sh
@@ -0,0 +1,20 @@
+
+brew install rust
+brew install go
+
+brew install choose
+brew install fd
+# brew install rg
+brew install fzf
+brew install bat
+brew install kubent
+
+brew install helix
+brew install git
+brew install lazygit
+brew install wezterm
+brew install starship
+
+
+# other tools 
+# https://github.com/dalance/amber - Text Search tool
