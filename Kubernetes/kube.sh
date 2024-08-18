b3e7b668c04315a302611301b581e478ad362e77
Vadim G <vadm.gagarin@ugmail.com>
2024-08-18 11:05:11 -0400

    added kube.sh to load argocd if docker is enabled

--- Kubernetes/kube.sh
+++ Kubernetes/kube.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env zsh
-export ARGOCD_CONTEXT=kind-kind
-export ARGOCD_USERNAME=admin
-export ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo)
-export ARGOCD_SERVER="argo-127-0-0-1.nip.io"
-alias argoin="argocd login $ARGOCD_SERVER --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD --grpc-web"
 
+if docker info > /dev/null 2>&1; then
+  echo "Docker is running.."
+  export ARGOCD_CONTEXT=kind-kind
+  export ARGOCD_USERNAME=admin
+  export ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo)
+  export ARGOCD_SERVER="argo-127-0-0-1.nip.io"
+  alias argoin="argocd login $ARGOCD_SERVER --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD --grpc-web"
+  # alias argoin="argocd login localhost:8080 --insecure --username admin --password $(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
+  export KUBE_EDITOR=hx
+ 
+fi
 
-export KUBE_EDITOR=hx
